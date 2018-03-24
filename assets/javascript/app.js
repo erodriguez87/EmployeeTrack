82a8a019c4d8cb05c31e95c8132215fd194078a5
SusanLDavenport <susanldavenport@gmail.com>
2018-03-24 10:53:21 -0400

    merge

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,16 @@
 
 // Wait for the page to finish loading
 $(document).ready(function() {
+
+  var config = {
+    apiKey: "AIzaSyDKWABfmD5z9i_HHVeWAbSxukH1yZqeoAE",
+    authDomain: "susangt2018.firebaseapp.com",
+    databaseURL: "https://susangt2018.firebaseio.com",
+    projectId: "susangt2018",
+    storageBucket: "susangt2018.appspot.com",
+    messagingSenderId: "271189265430"
+  };
+  firebase.initializeApp(config);
   
   var Name;
   var Role;
@@ -9,6 +19,12 @@ $(document).ready(function() {
   var Rate = 0;
   var Billed = 0;
   
-  
+  database.ref().push({
+    name: Name,
+    role: Role,
+    startDate: startDate,
+    rate: Rate
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
 
 });
